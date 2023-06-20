94da50b228aa7b8057a7a4214c0c99a88b5f3093
project-88288 <chaiya.88288@gmail.com>
2023-06-20 17:27:52 +0700

    update

---
+++ cw721/marketplace.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: "LunaPunks",
+    link: "https://lunapunks.io",
+    icon: "https://lunapunks.io/hero.jpg",
+  },
+  {
+    name: "Random Earth",
+    link: "https://randomearth.io",
+  },
+  {
+    name: "KNOWHERE",
+    link: "https://knowhere.art",
+    icon: "https://knowhere.art/static/logos/knowhere-logo.svg",
+  },
+  {
+    name: "Talis",
+    link: "https://talis.art",
+    icon: "https://talis.art/icons/talis/logo.svg",
+  },
+  {
+    name: "Miata",
+    link: "https://miata.io",
+    icon: "https://miata.io/logo192.png",
+  },
+];
