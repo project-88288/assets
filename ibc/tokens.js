7a42d3dad40d5608793a10a9ccaed6d0dbe73f4e
chaiya srisawat <chaiya@MSI>
2023-06-30 10:12:12 +0700

    fix

--- ibc/tokens.js
+++ ibc/tokens.js
@@ -1,5 +1,22 @@
 module.exports = {
-  mainnet: {},
+  mainnet: {
+    B3504E092456BA618CC28AC671A71FB08C6CA0FD0BE7C8A5B5A3E2DD933CC9E4: {
+      denom: "ibc/B3504E092456BA618CC28AC671A71FB08C6CA0FD0BE7C8A5B5A3E2DD933CC9E4",
+      path: "transfer/channel-6",
+      base_denom: "uusdc",
+      symbol: "axlUSDC",
+      name: "Axelar USD Coin",
+      icon: "https://assets.terra.money/icon/svg/ibc/axlUSDC.svg",
+    },
+    CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF: {
+      denom: "ibc/CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF",
+      path: "transfer/channel-6",
+      base_denom: "uusdt",
+      symbol: "axlUSDT",
+      name: "Axelar USD Tether",
+      icon: "https://assets.terra.money/icon/svg/ibc/axlUSDT.svg",
+    }
+  },
   classic: {},
   testnet: {},
 };
