6a79c74163b0cf62adf5fe2b59efd1923b0a35fa
chaiya srisawat <chaiya@MSI>
2023-06-30 10:16:41 +0700

    fix

--- cw20/tokens.js
+++ cw20/tokens.js
@@ -1,5 +1,14 @@
 module.exports = {
-  mainnet: {},
+  mainnet: {
+    terra1ry9f6alqyf9dpj04u9ymq5u4whjndu485agh6gusn89dmqse3ggsnzducj: {
+      protocol: "Optimizer Protocol",
+      symbol: "OPZA",
+      name: "Luna optimizer token",
+      token: "terra1ry9f6alqyf9dpj04u9ymq5u4whjndu485agh6gusn89dmqse3ggsnzducj",
+      icon: "https://avatars.githubusercontent.com/u/111164738?s=400&u=970d83fe90b55d4ca45e798fe0bc387d4ec5cbd5&v=4",
+      decimals: 6,
+    }
+  },
   classic: {},
   testnet: {},
 };
